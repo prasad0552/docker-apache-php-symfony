b78b7381d0e697f7bf02d574a2df61e3e3f8d472
VaraPrasad <prasad0552@gmail.com>
2020-08-12 03:18:26 +0530

    added symfony Java Articles, and Admin screens.

---
+++ src/Migrations/Version20200810021740.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types = 1);
+
+// phpcs:ignoreFile
+/** @noinspection PhpIllegalPsrClassPathInspection */
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200810021739 extends AbstractMigration
+{
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    /**
+     * @param Schema $schema
+     *
+     * @throws DBALException
+     */
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `symfony`.`java_article` CHANGE COLUMN `description` `description` MEDIUMTEXT NULL DEFAULT NULL, CHANGE COLUMN `code` `code` MEDIUMTEXT NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     *
+     * @throws DBALException
+     */
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `symfony`.`java_article` CHANGE COLUMN `description` `description` LONGTEXT NULL DEFAULT NULL, CHANGE COLUMN `code` `code` LONGTEXT NOT NULL');
+    }
+}
