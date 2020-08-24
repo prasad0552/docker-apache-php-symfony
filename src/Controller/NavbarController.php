b3b5eff4963ab165431b15ac03ff53835e502f83
VaraPrasad <prasad0552@gmail.com>
2020-08-25 02:30:03 +0530

    Menu Changes

--- src/Controller/NavbarController.php
+++ src/Controller/NavbarController.php
@@ -4,7 +4,38 @@
 namespace App\Controller;
 
 
+use KevinPapst\AdminLTEBundle\Event\NavbarUserEvent;
+use KevinPapst\AdminLTEBundle\Event\ShowUserEvent;
+use Symfony\Component\HttpFoundation\Response;
+
 class NavbarController extends \KevinPapst\AdminLTEBundle\Controller\NavbarController
 {
+    /**
+     * @return Response
+     */
+    public function userAction(): Response
+    {
+        if (!$this->hasListener(NavbarUserEvent::class)) {
+            return new Response();
+        }
 
+        /** @var ShowUserEvent $userEvent */
+        $userEvent = $this->dispatch(new NavbarUserEvent());
+
+        if ($userEvent instanceof ShowUserEvent && null !== $userEvent->getUser()) {
+            return $this->render(
+                '@AdminLTE/Navbar/user.html.twig',
+                [
+                    'user' => $userEvent->getUser(),
+                    'links' => $userEvent->getLinks(),
+                    'showProfileLink' => $userEvent->isShowProfileLink(),
+                    'showLogoutLink' => $userEvent->isShowLogoutLink(),
+                ]
+            );
+        } else {
+            return $this->render('Navbar/loginactions.html.twig');
+        }
+
+        return new Response();
+    }
 }
