4a17e87ff5df53518fa1dcc5bfc3b584bf0b6b30
VaraPrasad <prasad0552@gmail.com>
2020-08-25 20:09:15 +0530

    Nav header and no sidebar changes

--- src/Compiler/Strategy/JavaStrategy.php
+++ src/Compiler/Strategy/JavaStrategy.php
@@ -15,39 +15,37 @@ trait JavaStrategy
     public function compileJava($code, $compiler, $className = null)
     {
         $this->setCompiler($compiler);
-        $compiler_configs = $this->getCompilerConfigs($compiler);
-
-        $className = $className ?? $compiler_configs['main_class'];
+        $compilerConfig = $this->getCompilerConfigs($compiler);
+        $className = $className ?? $compilerConfig['main_class'];
         //save the code in a file with that extension
-        $file_name =  $this->getCompilationPath() . DS . $className . $compiler_configs['file_extension'] ;
+        $file_name = $this->getCompilationPath() . DS . $className . $compilerConfig['file_extension'];
         file_put_contents($file_name, $code);
-
         //run the command that compiles the code in that file
-        $command = $compiler_configs['path_compile'] . " " . $file_name . " 2>&1";
-
+        $command = $compilerConfig['path_compile'] . " " . $file_name . " 2>&1";
         //i did not use Core::runCommand because its not build for compilation
         exec($command, $output, $status);
-
-        return ( empty($output) ? true : $output );
+        return (empty($output) ? true : $output);
     }
 
     /**
      * This function will compile the java code
+     * @param null $className
      * @param null $input_file
      * @param null $output_file
-     * @return string
+     * @return string|null
      */
     public function runJava($className = null, $input_file = null, $output_file = null)
     {
-        $configs = $this->getCompilerConfigs($this->getCompiler());
-
-        $className = $className ?? $configs['main_class'];
-        $runtime_file="runtime.txt";
-
+        $compilerConfig = $this->getCompilerConfigs($this->getCompiler());
+        $className = $className ?? $compilerConfig['main_class'];
         if ($input_file == null && $output_file == null) {
             //get the class name and run it using java command
-            $command = $configs['path_run'] . " -classpath .:". $this->getCompilationPath() . DS . " " . $className . " 2>&1";
+            $command = $compilerConfig['path_run'] . " -classpath .:" . $this->getCompilationPath() . DS . " " . $className . " 2>&1";
             $output = shell_exec($command);
+
+            exec("rm " . $this->getCompilationPath() . DS . $className . ".class");
+            $classFile = $this->getCompilationPath() . DS . $className . $compilerConfig['file_extension'];
+            exec("rm $classFile");
             return $output;
         }
     }
