c2d37c34e2dea9638edc72231284b8d91c2b6ae7
VaraPrasad <prasad0552@gmail.com>
2020-08-29 00:40:35 +0530

    codepanel

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,17 +7,21 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+
+import CodeMirror from 'codemirror';
+
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/clike/clike'
+import 'codemirror/theme/monokai.css'
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
 // console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
-import CodeMirror from 'codemirror';
 
 CodeMirror.fromTextArea(document.getElementById("form_code"), {
     lineNumbers: true,
-    mode: "text/x-java"
+    mode: "clike",
+    theme: 'monokai'
 });
