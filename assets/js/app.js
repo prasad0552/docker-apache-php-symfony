781c1238afcbc656d918164fab6bdda10724dd13
VaraPrasad <prasad0552@gmail.com>
2020-08-29 07:37:05 +0530

    drag & drop menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,7 @@ import $ from 'jquery';
 
 document.addEventListener('DOMContentLoaded', (event) => {
     function handleDragStart(event) {
-        console.log(event.target.getAttribute("data-codesnippet"));
+        console.log(event.target);
         ev.dataTransfer.dropEffect = "copy";
         event.dataTransfer.effectAllowed = 'move';
         event.dataTransfer
